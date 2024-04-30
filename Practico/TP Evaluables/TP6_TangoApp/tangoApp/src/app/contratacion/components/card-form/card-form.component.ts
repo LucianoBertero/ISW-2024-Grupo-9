edcdac824553241f898472d7ab733543b2c59f4e
LucianoBertero <68975671+LucianoBertero@users.noreply.github.com>
2024-04-30 14:19:46 -0300

    validacion dni- pasaporte fallido

--- Practico/TP Evaluables/TP6_TangoApp/tangoApp/src/app/contratacion/components/card-form/card-form.component.ts
+++ Practico/TP Evaluables/TP6_TangoApp/tangoApp/src/app/contratacion/components/card-form/card-form.component.ts
@@ -28,13 +28,13 @@ export class CardFormComponent {
     ],
     pin: ['123', [Validators.required, Validators.pattern('^[0-9]{3}$')]],
     fechaExp: ['', [Validators.required]],
-    nroDoc: ['', [Validators.required, Validators.pattern('^[0-9]{4,15}$')]],
+    nroDoc: ['', [Validators.required]],
   });
+
+  // , Validators.pattern('^[0-9]{4,15}$')
 
   constructor(private fb: FormBuilder) {
     this.myForm.valueChanges.subscribe(() => {
-      console.log(this.myForm.controls['nroTarjeta'].value);
-
       if (
         this.myForm.controls['nroTarjeta'].value === 111111111111111 &&
         this.myForm.valid
@@ -57,6 +57,12 @@ export class CardFormComponent {
     return hoy.toISOString().split('T')[0]; // Formato: 'YYYY-MM-DD'
   }
 
+  isValidFieldNroDoc(field: string): boolean | null {
+    return (
+      this.myForm.controls[field].errors && this.myForm.controls[field].touched
+    );
+  }
+
   isValidField(field: string): boolean | null {
     return (
       this.myForm.controls[field].errors && this.myForm.controls[field].touched
@@ -68,6 +74,44 @@ export class CardFormComponent {
     if (!this.myForm.controls[field]) return null;
 
     const errors = this.myForm.controls[field].errors || {};
+
+    let customError: string | null = null; // Variable para almacenar el error personalizado
+    console.log(field);
+    if (field === 'nroDoc') {
+      console.log('entra');
+      const tipoDocValue = this.myForm.controls['tipoDoc'].value;
+      const nroDocValue = this.myForm.controls[field].value;
+
+      switch (tipoDocValue) {
+        case 'dni':
+          console.log('entro a DNI');
+          if (!/^\d{8}$/.test(nroDocValue)) {
+            customError = 'El DNI debe tener 8 dígitos.';
+          }
+          break;
+        case 'pasaporte':
+          if (!/^[A-Z]{2}\d{7}$/.test(nroDocValue)) {
+            customError =
+              'El pasaporte debe tener 2 letras seguidas de 7 dígitos.';
+          }
+          break;
+        case 'partNac':
+          if (!/^\d{4}-\d{4}-\d{6}$/.test(nroDocValue)) {
+            customError =
+              'El número de partida de nacimiento debe tener el formato YYYY-MM-DD-######.';
+          }
+          break;
+        case 'otro':
+          // Aquí puedes agregar la expresión regular y mensaje de error para otro tipo de documento
+          break;
+        default:
+          break;
+      }
+
+      if (customError) {
+        this.myForm.controls['nroDoc'].setErrors({ custom: customError });
+      }
+    }
 
     for (const key of Object.keys(errors)) {
       console.log(key);
@@ -92,6 +136,10 @@ export class CardFormComponent {
         case 'pattern':
           console.log('entro a maxlenght');
           return `Formato incorrecto`;
+
+        case 'custom':
+          console.log('entro a custom');
+          return `Error de Formato ${errors['custom']} .`;
       }
     }
 
